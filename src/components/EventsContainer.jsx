f4a28e97f057ea9af87e5ae3b3ab159b0e1940e1
Russell Lewis <russell.lewis@outlook.com>
2020-02-15 11:09:47 -0800

    prune some dead routes

--- src/components/EventsContainer.jsx
+++ src/components/EventsContainer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Event from "./Event";
-import EventGameListContainer from "./EventGameListContainer";
 
 export default function EventsContainer() {
   let match = useRouteMatch();
@@ -10,9 +9,6 @@ export default function EventsContainer() {
     <Switch>
       <Route exact path={`${match.path}/:eventId`}>
         <Event />
-      </Route>
-      <Route path={`${match.path}/:eventId/games`}>
-        <EventGameListContainer />
       </Route>
       <Route path={match.path}>
         <h3>Please select an Event</h3>
