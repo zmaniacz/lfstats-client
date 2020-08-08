a70a8665892c05bbad406db5107da9cd92d1cdfb
Russell Lewis <russell.lewis@outlook.com>
2020-08-07 17:28:44 -0700

    minor refactor

--- src/components/LeaderBoardPositionTable.jsx
+++ src/components/LeaderBoardPositionTable.jsx
@@ -1,3 +1,64 @@
 import React from "react";
+import { EuiInMemoryTable } from "@elastic/eui";
+import EuiCustomLink from "./EuiCustomLink";
 
-export default () => [];
+export default function LeaderBoardPositionTable({ position, data }) {
+  const columns = [
+    {
+      field: "player_name",
+      name: "Name",
+      dataType: "string",
+      sortable: false,
+      truncateText: true,
+      render: (name, item) => (
+        <EuiCustomLink to={`/players/${item.player.id}`}>
+          {item.player.active_player_name}
+        </EuiCustomLink>
+      ),
+    },
+    {
+      field: "score",
+      name: "Score",
+      dataType: "number",
+      sortable: true,
+      render: (name, item) => (
+        <EuiCustomLink to={`/games/${item.game.id}`}>{name}</EuiCustomLink>
+      ),
+    },
+    {
+      field: "mvp_points",
+      name: "MVP",
+      dataType: "number",
+      sortable: true,
+      render: (name, item) => (
+        <EuiCustomLink to={`/games/${item.game.id}`}>
+          {Number.parseFloat(name).toFixed(2)}
+        </EuiCustomLink>
+      ),
+    },
+  ];
+
+  const sorting = {
+    sort: {
+      field: "score",
+      direction: "desc",
+    },
+    allowNeutralSort: false,
+  };
+
+  const pagination = {
+    initialPageSize: 5,
+    hidePerPageOptions: true,
+  };
+
+  return (
+    <EuiInMemoryTable
+      columns={columns}
+      items={data}
+      search={false}
+      compressed={true}
+      pagination={pagination}
+      sorting={sorting}
+    />
+  );
+}
--- src/components/LeaderBoardPositions.jsx
+++ src/components/LeaderBoardPositions.jsx
@@ -1,116 +1,43 @@
 import React from "react";
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiInMemoryTable,
-  EuiText,
-} from "@elastic/eui";
-import EuiCustomLink from "./EuiCustomLink";
+import { EuiFlexGroup, EuiFlexItem, EuiText } from "@elastic/eui";
+import LeaderBoardPositionsTable from "./LeaderBoardPositionTable";
 
 export default ({ data }) => {
-  const columns = [
-    {
-      field: "player_name",
-      name: "Name",
-      dataType: "string",
-      sortable: false,
-      truncateText: true,
-      render: (name, item) => (
-        <EuiCustomLink to={`/players/${item.player.id}`}>
-          {item.player.active_player_name}
-        </EuiCustomLink>
-      ),
-    },
-    {
-      field: "score",
-      name: "Score",
-      dataType: "number",
-      sortable: true,
-      render: (name, item) => (
-        <EuiCustomLink to={`/games/${item.game.id}`}>{name}</EuiCustomLink>
-      ),
-    },
-    {
-      field: "mvp_points",
-      name: "MVP",
-      dataType: "number",
-      sortable: true,
-      render: (name, item) => (
-        <EuiCustomLink to={`/games/${item.game.id}`}>
-          {Number.parseFloat(name).toFixed(2)}
-        </EuiCustomLink>
-      ),
-    },
-  ];
-
-  const sorting = {
-    sort: {
-      field: "score",
-      direction: "desc",
-    },
-    allowNeutralSort: false,
-  };
-
-  const pagination = {
-    initialPageSize: 5,
-    hidePerPageOptions: true,
-  };
-
   return (
     <EuiFlexGroup wrap={true}>
       <EuiFlexItem grow={5}>
         <EuiText color="default">Commander</EuiText>
-        <EuiInMemoryTable
-          columns={columns}
-          items={data.filter((item) => item.position === "Commander")}
-          search={false}
-          compressed={true}
-          pagination={pagination}
-          sorting={sorting}
+        <LeaderBoardPositionsTable
+          position="Commander"
+          data={data.filter((item) => item.position === "Commander")}
         />
       </EuiFlexItem>
       <EuiFlexItem grow={5}>
         <EuiText color="default">Heavy Weapons</EuiText>
-        <EuiInMemoryTable
-          columns={columns}
-          items={data.filter((item) => item.position === "Heavy Weapons")}
-          search={false}
-          compressed={true}
-          pagination={pagination}
-          sorting={sorting}
+        <LeaderBoardPositionsTable
+          position="Heavy Weapons"
+          data={data.filter((item) => item.position === "Heavy Weapons")}
         />
       </EuiFlexItem>
       <EuiFlexItem grow={5}>
         <EuiText color="default">Scout</EuiText>
-        <EuiInMemoryTable
-          columns={columns}
-          items={data.filter((item) => item.position === "Scout")}
-          search={false}
-          compressed={true}
-          pagination={pagination}
-          sorting={sorting}
+        <LeaderBoardPositionsTable
+          position="Scout"
+          data={data.filter((item) => item.position === "Scout")}
         />
       </EuiFlexItem>
       <EuiFlexItem grow={5}>
         <EuiText color="default">Ammo Carrier</EuiText>
-        <EuiInMemoryTable
-          columns={columns}
-          items={data.filter((item) => item.position === "Ammo Carrier")}
-          search={false}
-          compressed={true}
-          pagination={pagination}
-          sorting={sorting}
+        <LeaderBoardPositionsTable
+          position="Ammo Carrier"
+          data={data.filter((item) => item.position === "Ammo Carrier")}
         />
       </EuiFlexItem>
       <EuiFlexItem grow={5}>
         <EuiText color="default">Medic</EuiText>
-        <EuiInMemoryTable
-          columns={columns}
-          items={data.filter((item) => item.position === "Medic")}
-          search={false}
-          compressed={true}
-          pagination={pagination}
-          sorting={sorting}
+        <LeaderBoardPositionsTable
+          position="Medic"
+          data={data.filter((item) => item.position === "Medic")}
         />
       </EuiFlexItem>
     </EuiFlexGroup>
